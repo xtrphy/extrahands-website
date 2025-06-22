e1e76243b1b73559667296ade5eb4d9d970105bb
xtrphy <perepeckinvlad@gmail.com>
2025-06-22 20:48:41 +0500

    styles: Add favicon and rubik font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            Home
+            <h1 className='text-3xl'>Домашняя страница</h1>
+            <h2 className='text-3xl'>EXTRA HANDS</h2>
         </div>
     );
 };
