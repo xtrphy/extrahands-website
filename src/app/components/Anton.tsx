eaf05b7ee6092d783224140f38be54bd38c20e30
xtrphy <perepeckinvlad@gmail.com>
2025-06-25 16:01:12 +0500

    styles: change mobile adaptation for Anton section

--- src/app/components/Anton.tsx
+++ src/app/components/Anton.tsx
@@ -26,14 +26,26 @@ const Separator = () => {
         offset: ['start end', 'end start'],
     });
 
+    const scaleMobile = useTransform(scrollYProgress, [0, 1], [1, 1.6]);
     const scaleDesktop = useTransform(scrollYProgress, [0, 1], [0.8, 1.2]);
-    const scaleMobile = useTransform(scrollYProgress, [0, 1], [1.25, 2.5]);
     const scale = isMobile ? scaleMobile : scaleDesktop;
 
-    const x1 = useTransform(scrollYProgress, [0, 1], [300, -300]);
-    const y1 = useTransform(scrollYProgress, [0.2, 1], [-100, 0]);
-    const x2 = useTransform(scrollYProgress, [0, 1], [-100, 100]);
-    const y2 = useTransform(scrollYProgress, [0.2, 1], [300, 300]);
+    const x1Mobile = useTransform(scrollYProgress, [0, 1], [100, -100]);
+    const x1Desktop = useTransform(scrollYProgress, [0, 1], [300, -300]);
+    const x1 = isMobile ? x1Mobile : x1Desktop;
+
+    const y1Mobile = useTransform(scrollYProgress, [0.2, 1], [-50, 0]);
+    const y1Desktop = useTransform(scrollYProgress, [0.2, 1], [-100, 0]);
+    const y1 = isMobile ? y1Mobile : y1Desktop;
+
+    const x2Mobile = useTransform(scrollYProgress, [0, 1], [-50, 50]);
+    const x2Desktop = useTransform(scrollYProgress, [0, 1], [0, 200]);
+    const x2 = isMobile ? x2Mobile : x2Desktop;
+
+    const y2Mobile = useTransform(scrollYProgress, [0.2, 1], [250, 250]);
+    const y2Desktop = useTransform(scrollYProgress, [0.2, 1], [200, 200]);
+    const y2 = isMobile ? y2Mobile : y2Desktop;
+
     const opacity2 = useTransform(scrollYProgress, [0.2, 0.8], [0, 1]);
 
     return (
@@ -61,7 +73,7 @@ const Separator = () => {
             </motion.h2>
 
             <motion.h2
-                style={{ x: x2, opacity: opacity2, y: y2 }}
+                style={{ x: x2, y: y2, opacity: opacity2 }}
                 className='text-5xl text-shadow-lg text-shadow-white/50'
             >
                 Или нет?...
