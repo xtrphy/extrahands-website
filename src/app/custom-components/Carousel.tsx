0238cff0eeda1b40a004e7d5a39a967903aa7f4a
xtrphy <perepeckinvlad@gmail.com>
2025-06-26 15:32:52 +0500

    feat: Finish Carousel section, add typewriter effect for h2 tag, add dots for navigation between screenshots

--- src/app/custom-components/Carousel.tsx
+++ src/app/custom-components/Carousel.tsx
@@ -1,10 +1,13 @@
 "use client"
+
 import * as React from "react"
+import { useState, useEffect } from "react"
 import Autoplay from "embla-carousel-autoplay"
 import Image from "next/image"
 
 import {
     Carousel,
+    CarouselApi,
     CarouselContent,
     CarouselItem,
     CarouselNext,
@@ -12,66 +15,99 @@ import {
 } from "@/components/ui/carousel"
 
 export default function CarouselPlugin() {
+    const [isDesktop, setIsDesktop] = useState(false);
+    const [api, setApi] = useState<CarouselApi>();
+    const [current, setCurrent] = useState(0);
     const plugin = React.useRef(
         Autoplay({ delay: 4000, stopOnInteraction: true })
-    )
+    );
+
+    useEffect(() => {
+        const checkDesktop = () => {
+            setIsDesktop(window.innerWidth > 768);
+        };
+
+        checkDesktop();
+        window.addEventListener('resize', checkDesktop);
+
+        return () => {
+            window.removeEventListener('resize', checkDesktop);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!api) return;
+
+        setCurrent(api.selectedScrollSnap());
+
+        const onSelect = () => {
+            setCurrent(api.selectedScrollSnap());
+        }
+
+        api.on("select", onSelect);
+        return () => {
+            api.off("select", onSelect);
+        }
+    }, [api]);
 
     const images = [
-        {
-            src: "/kholl.png",
-            alt: "Коридор",
-        },
-        {
-            src: "/kladovka.png",
-            alt: "Кладовка"
-        },
-        {
-            src: "/krovat.png",
-            alt: "Кровать"
-        },
-        {
-            src: "/poselok.png",
-            alt: "Посёлок"
-        },
-        {
-            src: "/shop.png",
-            alt: "Магазин"
-        },
-        {
-            src: "/tip.png",
-            alt: "Мальчик",
-        },
-        {
-            src: "/school.png",
-            alt: "Школа",
-        }
-    ]
+        { src: "/poselok.png", alt: "Посёлок" },
+        { src: "/Roditeli.png", alt: "Родители идут с коробками" },
+        { src: "/Anton_Olya_Hall.png", alt: "Антон и Оля в коридоре" },
+        { src: "/kladovka.png", alt: "Кладовка" },
+        { src: "/magazin.png", alt: "Магазин" },
+        { src: "/Roma_Byasha.png", alt: "Рома и Бяша" },
+        { src: "/school.png", alt: "Школа" },
+        { src: "/Fox.png", alt: "Лиса" },
+    ];
 
     return (
-        <Carousel
-            plugins={[plugin.current]}
-            className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl mx-auto"
-            onMouseEnter={plugin.current.stop}
-            onMouseLeave={plugin.current.reset}
-        >
-            <CarouselContent>
-                {images.map((image, index) => (
-                    <CarouselItem key={index}>
-                        <div className="relative aspect-video overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-                            <Image
-                                src={image.src}
-                                alt={image.alt}
-                                fill
-                                className="object-cover transition-transform hover:scale-102 duration-300"
-                                priority={index === 0}
-                                sizes="(max-width: 640px) 90vw, (max-width: 768px) 80vw, (max-width: 1024px) 70vw, (max-width: 1280px) 60vw, 50vw"
-                            />
-                        </div>
-                    </CarouselItem>
+        <>
+            <Carousel
+                plugins={[plugin.current]}
+                className="w-full max-w-md sm:max-w-sm md:max-w-xl lg:max-w-3xl xl:max-w-xl 2xl:max-w-4xl mx-auto"
+                onMouseEnter={plugin.current.stop}
+                onMouseLeave={plugin.current.reset}
+                setApi={setApi}
+            >
+                <CarouselContent>
+                    {images.map((image, index) => (
+                        <CarouselItem key={index}>
+                            <div className="relative aspect-video overflow-hidden rounded-lg transition-shadow duration-300">
+                                <Image
+                                    src={image.src}
+                                    alt={image.alt}
+                                    fill
+                                    className="object-cover transition-transform hover:scale-102 duration-300"
+                                    priority={index === 0}
+                                    sizes="(max-width: 640px) 90vw, (max-width: 768px) 80vw, (max-width: 1024px) 70vw, (max-width: 1280px) 60vw, 50vw"
+                                />
+                            </div>
+
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                {isDesktop && (
+                    <>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </>
+                )}
+            </Carousel>
+
+            <div className="flex justify-center mt-5 gap-2">
+                {images.map((_, index) => (
+                    <button
+                        key={index}
+                        className={`h-2.25 w-2.25 rounded-full transition-all duration-300 cursor-pointer ${current === index
+                            ? "bg-[#7b7b7b] scale-125"
+                            : "bg-white hover:brightness-50"
+                            }`}
+                        onClick={() => api?.scrollTo(index)}
+                        aria-label={`Перейти к слайду ${index + 1}`}
+                    />
                 ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-        </Carousel>
-    )
+            </div>
+        </>
+    );
 }
--- src/app/custom-components/Screenshots.tsx
+++ src/app/custom-components/Screenshots.tsx
@@ -1,21 +1,32 @@
-'use client';
+"use client";
 
-import React from 'react';
-import CarouselPlugin from '@/app/custom-components/Carousel';
-import { motion } from 'framer-motion';
+import React from "react";
+import CarouselPlugin from "@/app/custom-components/Carousel";
+import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
 
 const Screenshots = () => {
     return (
-        <section className='flex flex-col font-advaken text-white items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8'>
+        <section className="flex flex-col font-advaken text-white items-center justify-center min-h-screen px-4 sm:px-6 lg:px-8">
             <motion.div
                 initial={{ opacity: 0, y: -50 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, ease: 'easeInOut' }}
+                transition={{ duration: 1, ease: "easeInOut" }}
                 viewport={{ once: true, amount: 0.2 }}
-                className='w-full max-w-7xl mx-auto text-center'
+                className="w-full max-w-7xl mx-auto text-center"
             >
-                <h2 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl mb-6 sm:mb-8 lg:mb-10 px-2 leading-tight'>Нужно начать всё с чистого листа...</h2>
-                <div className='w-full flex justify-center'>
+                <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl mb-6 sm:mb-8 lg:mb-10 px-2 leading-tight">
+                    <Typewriter
+                        words={["Нужно начать всё с чистого листа...", "Но всё ли так просто?", "Влияю ли я на что либо?", "", "Нет..."]}
+                        loop={true}
+                        cursor
+                        cursorStyle="|"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1500}
+                    />
+                </h2>
+                <div className="w-full flex flex-col justify-center">
                     <CarouselPlugin />
                 </div>
             </motion.div>
