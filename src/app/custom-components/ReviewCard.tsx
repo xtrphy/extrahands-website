2434e43229931e185e4f90fd8ffa4a4d9b825092
xtrphy <perepeckinvlad@gmail.com>
2025-06-29 16:26:19 +0500

    Adapt reviews section, add show more button

--- src/app/custom-components/ReviewCard.tsx
+++ src/app/custom-components/ReviewCard.tsx
@@ -10,6 +10,7 @@ interface ReviewProps {
 
 const AvatarComponent = ({ avatar, nickname }: { avatar: string, nickname: string }) => {
     const firstTwoLetters = nickname.slice(0, 2);
+
     return (
         <Avatar>
             <AvatarImage src={avatar} />
@@ -20,16 +21,11 @@ const AvatarComponent = ({ avatar, nickname }: { avatar: string, nickname: strin
 
 const ReviewCard = ({ review }: { review: ReviewProps }) => {
     return (
-        <div
-            className="max-w-sm border border-gray-300 rounded-lg shadow-lg bg-white p-6 space-y-3 hover:scale-103 transition-transform duration-250"
+        <article
+            className="w-full sm:w-[300px] md:w-[340px] lg:w-[360px] border border-gray-300 rounded-lg shadow-lg bg-white p-6 space-y-3 hover:scale-103 transition-transform duration-250"
         >
             <div className="flex items-center space-x-4">
-                <div
-                    className="h-12 w-12 flex items-center justify-center bg-gray-400 text-white text-lg font-semibold rounded-full"
-                >
-                    <AvatarComponent avatar={review.avatar} nickname={review.nickname} />
-
-                </div>
+                <AvatarComponent avatar={review.avatar} nickname={review.nickname} />
                 <div className="text-gray-900 font-medium">{review.nickname}</div>
             </div>
 
@@ -42,7 +38,7 @@ const ReviewCard = ({ review }: { review: ReviewProps }) => {
             <Link href="https://vk.com/extra__hands?from=groups&w=wall-221086064_2786" target="_blank" className="text-red-500 font-medium cursor-pointer hover:underline">
                 Читать ещё
             </Link>
-        </div>
+        </article>
 
     );
 };
